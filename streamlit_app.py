409c026cb72dabcf88e54f004464c57e440abb26
Tanvi Kumari <63553233+04tanvi@users.noreply.github.com>
2023-10-18 22:54:01 +0530

    Update streamlit_app.py
    Add title,text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title("my parents")
+streamlit.title("Breakfast Menu")
+streamlit.header("Omega3 and blueberry Oatmeal")
+streamlit.text("Kale,Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
