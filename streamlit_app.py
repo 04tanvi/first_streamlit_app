7b03c3080e7898298831a805b98d4e29d4da1ce9
Tanvi Kumari <63553233+04tanvi@users.noreply.github.com>
2023-10-19 21:09:02 +0530

    Update streamlit_app.py
    Added emoji and extra header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.title("Breakfast Menu")
-streamlit.header("Omega3 and blueberry Oatmeal")
-streamlit.text("Kale,Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.title("🥣 Breakfast Menu")
+streamlit.header("🥗Omega3 and blueberry Oatmeal")
+streamlit.text(" 🐔Kale,Spinach & Rocket Smoothie")
+streamlit.text(" 🥑🍞Hard-Boiled Free-Range Egg")
+streamlit.text(" 🍞Avacado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
