54b82efac85a286abe84ab00a857d98dbe870505
Tanvi Kumari <63553233+04tanvi@users.noreply.github.com>
2023-10-20 12:28:33 +0530

    Update streamlit_app.py
    add as per choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
 
